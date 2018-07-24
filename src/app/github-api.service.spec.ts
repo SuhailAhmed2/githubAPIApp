9d6d4108bed3cc63b904e96a783e77b659826072
Suhail Ahmed <suhail.23.ahmed@gmail.com>
2018-07-24 18:28:34 +0530

    this spec yet to complete

--- src/app/github-api.service.spec.ts
+++ src/app/github-api.service.spec.ts
@@ -2,14 +2,50 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { GithubApiService } from './github-api.service';
 
-describe('GithubApiService', () => {
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+
+
+fdescribe('GithubApiService', () => {
+
+  let httpServiceSpy: jasmine.SpyObj<HttpClient>;
+  let githubApiServ: GithubApiService;
+
+
   beforeEach(() => {
+
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
+
     TestBed.configureTestingModule({
-      providers: [GithubApiService]
+      providers: [GithubApiService,{provide:HttpClient,useValue:spy}]
     });
+
+    githubApiServ = TestBed.get(GithubApiService);
+    httpServiceSpy = TestBed.get(HttpClient);
+
   });
 
-  it('should be created', inject([GithubApiService], (service: GithubApiService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('#getUsersData should return stubbed value from a spy', () => {
+    const stubValue = of('stub value');
+    httpServiceSpy.get.and.returnValue(stubValue);
+
+    const pipeSpy = jasmine.createSpyObj('Observable', ['pipe']);
+    pipeSpy.pipe.and.returnValue('pipe value');
+
+    githubApiServ.getUsersData().subscribe(res=>{
+
+      expect(res)
+      .toBe('stub value', 'service returned stub value');
+
+    });
+  
+    
+    // expect(httpServiceSpy.get.calls.count())
+    //   .toBe(1, 'spy method was called once');
+    // expect(httpServiceSpy.get.calls.mostRecent().returnValue)
+    //   .toBe(stubValue);
+  });
+
+ 
 });
